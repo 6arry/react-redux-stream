b6e86095c82033fc0c5241dcca0e511c6d1cf7e0
gccunanan <gccunanan@gmail.com>
2020-01-03 11:56:06 -0800

    initial commit; setting up redux to app

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -23,12 +23,12 @@ class GoogleAuth extends Component {
     }
 
     // Helper method signs in a user with Google
-    onSignIn = () => {
+    onSignInClick = () => {
         this.auth.signIn()
     }
 
     // Helper method signs out a user with Google
-    onSignOut = () => {
+    onSignOutClick = () => {
         this.auth.signOut()
     }
 
@@ -38,14 +38,14 @@ class GoogleAuth extends Component {
             return null
         } else if (this.state.isSignedIn){
             return (
-                <button onClick={this.onSignOut} className="ui red google button">
+                <button onClick={this.onSignOutClick} className="ui red google button">
                     <i className="google icon" />
                     Sign Out
                 </button>
             )
         } else {
             return (
-                <button onClick={this.onSignIn} className="ui red google button">
+                <button onClick={this.onSignInClick} className="ui red google button">
                     <i className="google icon" />
                     Sign In with Google
                 </button>
